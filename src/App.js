041f9f58e8fb689b8ff00631e52dd030969b2927
offiahchiaz <offiahchiaz@gmail.com>
2021-12-31 07:31:40 +0100

    Added ProjectList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/layout/Navbar';
+import Dashboard from './Components/dashboard/Dashboard';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/" component={ Dashboard } />
+        </Switch>
       </div>
     </Router>
   );
