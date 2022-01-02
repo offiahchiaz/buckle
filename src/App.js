0e57698b2dd9b77f91b3638e0b6bde4c50cd2624
offiahchiaz <offiahchiaz@gmail.com>
2022-01-02 02:30:04 +0100

    Added ProjectDetails component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './Components/layout/Navbar';
-import Dashboard from './Components/dashboard/Dashboard';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/" component={ Dashboard } />
+          <Route exact path="/" component={ Dashboard } />
+          <Route path="/project/:id" component={ ProjectDetails } />
         </Switch>
       </div>
     </Router>
---
+++ src/Components/projects/ProjectDetails.js
@@ -0,0 +1,21 @@
+const ProjectDetails = () => {
+  return ( 
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae, natus! 
+            Sunt nobis autem tempore. Doloribus, commodi aperiam adipisci inventore esse 
+            consequatur quibusdam! Deleniti distinctio libero sapiente recusandae fugit. 
+            Reiciendis, ratione.</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by The Net Ninja</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default ProjectDetails;
